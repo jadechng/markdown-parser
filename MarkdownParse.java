ad683f88973c5c340fa444b943b4b603342b9b0e
jadechng <103202818+jadechng@users.noreply.github.com>
2022-04-22 16:28:02 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -26,6 +26,7 @@ public class MarkdownParse {
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
             
+            //considers infinite loop caused by empty line
             if (!markdown.substring(closeParen).contains("[")){
                 break;
             }
